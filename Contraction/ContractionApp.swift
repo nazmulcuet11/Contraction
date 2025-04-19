48426bfcd0f64f98c1b5ad8b8a1ca3ae56a16a95
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-04-19 19:28:56 +0700

    Initial Commit

---
+++ Contraction/ContractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContractionApp.swift
+//  Contraction
+//
+//  Created by Nazmul Islam on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ContractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
